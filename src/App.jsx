231c5a7f25992d82d57c57a13f25bf1daba3218f
miguelHenrique390 <miguel.santos27012009@gmail.com>
2026-08-14 14:31:06 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,156 @@
+import { useState } from 'react';
+import "./App.css";
+
+
 function App() {
-    return(
-        <div>
-            <h1>projeto teste</h1>
+    const [texto, setTexto] = useState("");
+    const [tarefas, setTarefas] = useState([]);
+
+    function adicionarTarefa() {
+        if (texto.trim() !== "") {
+            const tarefaExistente = tarefas.find(tarefa => tarefa.texto === texto);
+
+
+            if (tarefaExistente) {
+                alert("Essa tarefa já foi adicionada!")
+                return;
+            }
+
+
+
+
+            const tarefa = {
+                texto: texto,
+                concluida: false
+            }
+
+            setTarefas([...tarefas, tarefa])
+
+            setTexto("")
+
+
+        }
+
+    }
+
+    function limparTarefas() {
+        setTarefas([])
+    }
+
+    function removerTarefa(indiceRemover) {
+        const listaAtualizada = tarefas.filter(
+            (tarefa, indice) => indice !== indiceRemover
+        )
+
+        setTarefas(listaAtualizada)
+    }
+
+
+
+    function concluirTarefa(indiceSelecionado) {
+        const listaAtualizada = tarefas.map(
+            (tarefa, indice) => {
+                if (indice === indiceSelecionado) {
+                    return {
+                        ...tarefa,
+                        concluida: !tarefa.concluida
+                    }
+
+                }
+                return tarefa
+
+            }
+        )
+        setTarefas(listaAtualizada)
+    }
+
+
+
+
+    return (
+        <div className="container">
+            <h1>Lista de tarefas</h1>
+
+            <div className="formulario">
+
+
+
+                <input
+                    type="text"
+                    value={texto}
+                    onChange={(e) => setTexto(e.target.value)}
+                    placeholder="Digite uma tarefa"
+                    onKeyDown={(e) => {
+
+                        if (e.key === "Enter") {
+                            adicionarTarefa();
+                        }
+                    }}
+                ></input>
+
+
+
+
+                <button onClick={adicionarTarefa}>Adicionar tarefa</button>
+            </div>
+
+
+            <p className="digitado">Você digitou: {texto}</p>
+
+
+            {
+                tarefas.length === 0 && (
+                    <p className="">Nenhuma tarefa adicionada.</p>
+                )
+            }
+
+            <ul className="lista">
+                {
+                    tarefas.map((tarefa, indice) => (
+                        <li className="item"
+                            key={indice}>
+                                <span
+                                className={
+                                    tarefa.concluida ? "concluida" : ""
+                                }>{tarefa.texto}</span>
+
+                                <div className="acoes">
+                                    <button onClick={() => concluirTarefa(indice)}>
+                                        {tarefa.concluida ? "Desfazer" : "concluir"}
+                                    </button>
+
+                                    <button onClick={() => removerTarefa(indice)}>
+                                        Remover
+                                    </button>
+                                </div>
+
+
+
+                            
+                        </li>
+                    ))
+                }
+
+
+
+
+            </ul>
+
+
+            <p>Total de tarefas: {tarefas.length}</p>
+
+            <button onClick={limparTarefas}>Limpar Tarefas</button>
+
+
         </div>
     )
+
+
+    return (
+        <div>
+        </div>
+
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from "React";
 import ReactDOM from "react-dom/client";
 import App from "./App";
- 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render( 
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
