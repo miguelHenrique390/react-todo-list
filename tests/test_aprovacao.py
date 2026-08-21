a698f763d18f4e638d87e4aa129c61deda6aeea2
miguelHenrique390 <miguel.santos27012009@gmail.com>
2026-08-21 16:39:45 -0300

    Delete tests directory

--- tests/test_aprovacao.py
+++
@@ -1,9 +0,0 @@
-from aprovar_media import verificar_aprovacao
-def calc_aprov_1():
-    assert verificar_aprovacao(7) == "aprovado"
-
-def calc_aprov_2():
-    assert verificar_aprovacao(3) == "reprovado"
-
-def calc_aprov_3():
-    assert verificar_aprovacao(6,5) == "recuperação"
--- tests/test_area.py
+++
@@ -1,9 +0,0 @@
-from calculo_area import calcular_area
-def test_area_1():
-    assert calcular_area(2, 9) == 18
-
-def test_area_2():
-    assert calcular_area(3,8) == 24
-
-def test_area_3():
-    assert calcular_area(4, 7) == 28
